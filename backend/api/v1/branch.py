d2122a61b38c160301c66dfc7c304bf2633a4bb8
xerottin <xerottin@gmail.com>
2025-07-21 16:58:55 +0500

    fix(branch): testest success

--- backend/api/v1/branch.py
+++ backend/api/v1/branch.py
@@ -5,21 +5,22 @@ from crud.branch import create_branch, update_owner_role_branch
 from db.session import get_pg_db
 from dependencies.auth import require_admin_or_company
 from models import User
+from schemas.branch import BranchInDb, BranchCreate
 from schemas.company import CompanyCreate, CompanyInDb
 
 router = APIRouter(prefix="", tags=["Branches"])
 
 
-@router.post("/", response_model=CompanyInDb, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=BranchInDb, status_code=status.HTTP_201_CREATED)
 async def create_branch_endpoint(
-        data: CompanyCreate,
+        data: BranchCreate,
         db: AsyncSession = Depends(get_pg_db),
         current_user: User = Depends(require_admin_or_company)
 ):
     return await create_branch(db, data)
 
 
-@router.patch("/{branch_id}", response_model=CompanyInDb, status_code=status.HTTP_200_OK)
+@router.patch("/{branch_id}", response_model=BranchInDb, status_code=status.HTTP_200_OK)
 async def add_owner_branch(
         branch_id: int,
         owner_id: int,
--- backend/schemas/branch.py
+++ backend/schemas/branch.py
@@ -4,15 +4,16 @@ from pydantic import BaseModel
 
 
 class BranchBase(BaseModel):
-    id: int
     username: str
     url: Optional[str] = None
     latitude: Optional[float] = None
     longitude: Optional[float] = None
     rating: Optional[float] = None
-    user_id: int
     class Config:
         orm_mode = True
 
 class BranchCreate(BranchBase):
+    pass
+
+class BranchInDb(BranchBase):
     pass
