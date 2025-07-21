ab561ed2e9ba7347cb8cfcfae5a893eb99d0c539
xerottin <xerottin@gmail.com>
2025-07-21 14:40:45 +0500

    feat(company): add_owner_company api

--- backend/api/v1/company.py
+++ backend/api/v1/company.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from crud.company import create_company
+from crud.company import create_company, update_owner_role
 from db.session import get_pg_db
 from dependencies.auth import require_admin
 from models import User
@@ -12,8 +12,18 @@ router = APIRouter(prefix="", tags=["Companies"])
 
 @router.post("/", response_model=CompanyInDb, status_code=status.HTTP_201_CREATED)
 async def create_company_endpoint(
-    data: CompanyCreate,
-    db: AsyncSession = Depends(get_pg_db),
-    current_user: User = Depends(require_admin)
+        data: CompanyCreate,
+        db: AsyncSession = Depends(get_pg_db),
+        current_user: User = Depends(require_admin)
 ):
     return await create_company(db, data)
+
+
+@router.patch("/{company_id}", response_model=CompanyInDb, status_code=status.HTTP_200_OK)
+async def add_owner_company(
+        company_id: int,
+        owner_id: int,
+        current_user: User = Depends(require_admin),
+        db: AsyncSession = Depends(get_pg_db)
+):
+    return await update_owner_role(company_id, owner_id, db)
--- backend/crud/company.py
+++ backend/crud/company.py
@@ -15,3 +15,20 @@ async def create_company(db: AsyncSession, data: CompanyCreate) -> Company:
     await db.commit()
     await db.refresh(company)
     return company
+
+async def update_owner_role( company_id: int, owner_id: int, db: AsyncSession):
+    query = await db.execute(select(Company).where(Company.id == company_id))
+    company = query.scalar_one_or_none()
+    if not company or company.is_active == False:
+        raise HTTPException(status_code=403, detail="Company does not exist")
+
+    if company.owner_id == owner_id:
+        return company
+
+    company.owner_id = owner_id
+
+    db.add(company)
+    await db.commit()
+    await db.refresh(company)
+    return company
+
